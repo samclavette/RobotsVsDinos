1677edf199f9870ba8d527510dc5049734066b82
Sam Clavette <samclavette@yahoo.com>
2020-07-13 14:40:55 -0500

    three dino instantiations in herd class

--- Herd.cs
+++ Herd.cs
@@ -8,5 +8,8 @@ namespace RobotsVDinos
 {
     class Herd
     {
+        Dinosaur dinoOne = new Dinosaur("T-Rex", 250, 100, 75);
+        Dinosaur dinoTwo = new Dinosaur("Raptor", 100, 25, 20);
+        Dinosaur dinoThree = new Dinosaur("Mosasaurus", 200, 75, 50);
     }
 }
