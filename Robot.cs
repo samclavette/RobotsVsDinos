66ed12a9e84e199c8496f8af43a8e459e3cff9fa
Sam Clavette <samclavette@yahoo.com>
2020-07-13 13:16:55 -0500

    robot calss variables/constructor

--- Robot.cs
+++ Robot.cs
@@ -8,5 +8,24 @@ namespace RobotsVDinos
 {
     class Robot
     {
+        // mem variables (has a)
+        string name;
+        int robotHealth;
+        int robotPowerLevel;
+        bool isAlive;
+        public Weapon weapon;
+
+        // constructor (spawner)
+
+        public Robot(string name, int robotHealth, int robotPowerLevel)
+        {
+            this.name = name;
+            this.robotHealth = robotHealth;
+            this.robotPowerLevel = robotPowerLevel;
+            this.isAlive = true;
+        }
+
+        //mem methods (can do)
+
     }
 }
